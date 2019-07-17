9ee77cc631402e60cdb92c2f9bc00c351f2ba8e9
janvenulet <37171903+janvenulet@users.noreply.github.com>
2019-07-17 11:20:29 +0200

    start method override

--- src/main/java/com/venul/betacom/microservice/MainVerticle.java
+++ src/main/java/com/venul/betacom/microservice/MainVerticle.java
@@ -8,8 +8,24 @@ public class MainVerticle extends AbstractVerticle
 
 	@Override
 	public void start(Future<Void> startFuture) throws Exception {
-		
-		super.start(startFuture);
+		Future<Void> steps = setupDatabase().compose(v -> setupHttpServer());
+		steps.setHandler(asyncResult -> {
+			if(asyncResult.succeeded()) {
+				startFuture.complete();
+			} else {
+				startFuture.fail(asyncResult.cause().toString());
+			}
+		});
+	}
+
+	private Future<Void> setupHttpServer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private Future<Void> setupDatabase() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
