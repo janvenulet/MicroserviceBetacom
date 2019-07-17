60ea19e70a83f90dac8b6068327b1e235b4f274c
janvenulet <37171903+janvenulet@users.noreply.github.com>
2019-07-17 12:01:42 +0200

    setupHttpServer method

--- src/main/java/com/venul/betacom/microservice/MainVerticle.java
+++ src/main/java/com/venul/betacom/microservice/MainVerticle.java
@@ -2,14 +2,21 @@ package com.venul.betacom.microservice;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.http.HttpServer;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.templ.freemarker.FreeMarkerTemplateEngine;
 
 public class MainVerticle extends AbstractVerticle
 {
 
+	private FreeMarkerTemplateEngine templateEngine;
+	
 	@Override
 	public void start(Future<Void> startFuture) throws Exception {
-		Future<Void> steps = setupDatabase().compose(v -> setupHttpServer());
-		steps.setHandler(asyncResult -> {
+		Future<Void> future = setupDatabase().compose(v -> setupHttpServer());
+		future.setHandler(asyncResult -> {
 			if(asyncResult.succeeded()) {
 				startFuture.complete();
 			} else {
@@ -19,6 +26,27 @@ public class MainVerticle extends AbstractVerticle
 	}
 
 	private Future<Void> setupHttpServer() {
+		Future <Void> future = Future.future();
+		HttpServer server = vertx.createHttpServer();
+		
+		Router router = Router.router(vertx);
+		
+		router.get("/").handler(indexHandler()); //error possibility
+		
+		templateEngine = FreeMarkerTemplateEngine.create(vertx); //??? 
+		server.requestHandler(router).listen(8092, asyncResult -> {
+		if (asyncResult.succeeded()) {
+			System.out.print("HTTP server running on port 8080");
+			future.complete();
+		} else {
+			System.out.print("Could not start a HTTP server\n" + asyncResult.cause().toString());
+			future.fail(asyncResult.cause());
+		}
+		});
+		return future;
+	}
+
+	private Handler<RoutingContext> indexHandler() {
 		// TODO Auto-generated method stub
 		return null;
 	}
